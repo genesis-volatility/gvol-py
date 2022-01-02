1264eb7c8a87766dff0ddb86f88086b95f6483ec
Denys Halenok <denys.halenok@gmail.com>
2022-01-02 22:20:40 +0100

    Add a simple smoke test

--- tests/test_gvol.py
+++ tests/test_gvol.py
@@ -1,5 +1,9 @@
-from gvol import __version__
+from gvol import GVol, __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_client_initialization():
+    _ = GVol("gvol_api_key")
