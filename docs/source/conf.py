0cdcb2d5058cebaef57ad751cefd9cf923f5ee77
Denys Halenok <denys.halenok@gmail.com>
2022-01-05 16:49:05 +0100

    Add type for exclude_patterns

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,8 @@
 
 # -- Project information -----------------------------------------------------
 
+from typing import List
+
 project = "GVol"
 copyright = "2022, Denys Halenok"
 author = "Denys Halenok"
@@ -44,7 +46,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns: List[str] = []
 
 
 # -- Options for HTML output -------------------------------------------------
