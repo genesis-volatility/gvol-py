d60149c77e4f995fb250323144c54a68f7d552cd
Pat Doyle <patrick.doyle126@gmail.com>
2022-02-12 15:35:35 -0600

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Denys Halenok"
 author = "Denys Halenok"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.22"
+release = "0.2.23"
 
 
 # -- General configuration ---------------------------------------------------
--- gvol/__init__.py
+++ gvol/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ("__version__", "GVol")
-__version__ = "0.2.22"
+__version__ = "0.2.23"
 
 from gvol.client import GVol
