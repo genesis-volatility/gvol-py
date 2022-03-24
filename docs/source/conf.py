64dc455b6ad0a1b97aacd9e55247f522d86c323a
Pat Doyle <patrick.doyle126@gmail.com>
2022-03-24 14:03:12 -0500

    adding stuff

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Denys Halenok"
 author = "Denys Halenok"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.4"
+release = "0.2.31"
 
 
 # -- General configuration ---------------------------------------------------
