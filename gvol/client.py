e874aa38bc86b361b696eeec749a7fa63ca9576e
Fabio Bassani <fabio.bassani@MacBookPro.local>
2022-05-18 13:47:00 +0200

    symbol/exchange descriptors

--- gvol/client.py
+++ gvol/client.py
@@ -2281,10 +2281,7 @@ class GVol:
         """
       Explanation:
         Inputs:
-        Exchange: Deribit, Bit.com, Okexkaljsdfhkljashd flkjahsd;lkj 
-
-        asd'lfoja;lsdkjv
-        ap[aosdjf;lkajsdf;lkajdf]
+        Exchange: Deribit, Bit.com, Okex
         Currency Symbol: BTC, ETH
 
         Why do traders like this endpoint?
