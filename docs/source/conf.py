f0328c0968089b4e342fdba9f17263093a14fb45
Pat Doyle <patrick.doyle126@gmail.com>
2022-06-10 23:06:26 -0500

    adding

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Denys Halenok"
 author = "Denys Halenok"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.4"
+release = "0.3.5"
 
 # -- General configuration ---------------------------------------------------
 
--- gvol/__init__.py
+++ gvol/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ("__version__", "GVol")
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 from gvol.client import GVol
