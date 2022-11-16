d6f3e2e2e76181d5ec4edd162f4387e325058ced
Pat Doyle <patdoyle@Pats-MacBook-Pro.local>
2022-11-16 12:05:02 -0600

    versioning

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Denys Halenok"
 author = "Denys Halenok"
 
 # The full version, including alpha/beta/rc tags
-release = "0.4.7"
+release = "0.4.8"
 
 # -- General configuration ---------------------------------------------------
 
